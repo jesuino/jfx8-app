1709550c5e0437569f0126bd12b48d008ea43de3
jesuino <william.a.siqueira@gmail.com>
2014-03-26 15:50:49 -0300

    No need of a main method in Java 8

--- src/main/java/org/jugvale/App.java
+++ src/main/java/org/jugvale/App.java
@@ -7,9 +7,6 @@ import javafx.scene.layout.StackPane;
 
 public class App extends Application{
 
-	public static void main(String args[]){
-		launch(args);
-	}
 
 	@Override
 	public void start(Stage stage){
